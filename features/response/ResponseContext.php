e4111218e3046d24b1bb9fcbdc3cefad75c74e87
Matt Henderson <matt_henderson@sil.org>
2017-03-01 14:32:54 -0500

    Add basic response tests.

--- features/response/ResponseContext.php
+++ features/response/ResponseContext.php
@@ -1,11 +1,104 @@
 <?php
 namespace Sil\Idp\IdBroker\Client\features\response;
 
+use Behat\Gherkin\Node\PyStringNode;
+use Behat\Behat\Tester\Exception\PendingException;
 use Behat\Behat\Context\Context;
+use GuzzleHttp\Handler\MockHandler;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Middleware;
+use GuzzleHttp\Psr7\Response;
+use PHPUnit\Framework\Assert;
+use Sil\Idp\IdBroker\Client\IdBrokerClient;
 
 /**
  * Defines application features from the specific context.
  */
 class ResponseContext implements Context
 {
+    private $methodName;
+    private $response = null;
+    private $result;
+    
+    protected function getHttpClientHandlerForTests()
+    {
+        Assert::assertNotEmpty(
+            $this->response,
+            'You need to define the response before you can pretend to call the API.'
+        );
+        $mockHandler = new MockHandler([$this->response]);
+        return HandlerStack::create($mockHandler);
+    }
+    
+    /**
+     * @return IdBrokerClient
+     */
+    protected function getIdBrokerClient()
+    {
+        return new IdBrokerClient('https://api.example.com/', 'DummyAccessToken', [
+            'http_client_options' => [
+                'handler' => $this->getHttpClientHandlerForTests(),
+            ],
+        ]);
+    }
+
+    /**
+     * @Given a call to :methodName will return a :statusCode response with following data:
+     */
+    public function aCallToWillReturnAResponseWithFollowingData(
+        $methodName,
+        $statusCode,
+        PyStringNode $responseData
+    ) {
+        $this->methodName = $methodName;
+        $this->response = new Response($statusCode, [], (string)$responseData);
+    }
+
+    /**
+     * @Then the status code should be :expectedStatusCode
+     */
+    public function theStatusCodeShouldBe($expectedStatusCode)
+    {
+        Assert::assertNotEmpty($this->result);
+        Assert::assertEquals($this->result['statusCode'], $expectedStatusCode);
+    }
+
+    /**
+     * @When I call getUser
+     */
+    public function iCallGetuser()
+    {
+        $this->result = $this->getIdBrokerClient()->getUser([
+            'employee_id' => '123',
+        ]);
+    }
+
+    /**
+     * @Given a call to :methodName will return a :statusCode response
+     */
+    public function aCallToWillReturnAResponse($methodName, $statusCode)
+    {
+        $this->methodName = $methodName;
+        $this->response = new Response($statusCode);
+    }
+
+    /**
+     * @When I call it with a(n) :field of :value
+     */
+    public function iCallItWithAnOf($field, $value)
+    {
+        $methodName = $this->methodName;
+        $this->result = $this->getIdBrokerClient()->$methodName([
+            $field => $value,
+        ]);
+    }
+
+    /**
+     * @Then the resulting :field should be :value
+     */
+    public function theResultingShouldBe($field, $value)
+    {
+        Assert::assertNotEmpty($this->result);
+        Assert::assertSame($this->result[$field], $value);
+    }
 }
--- features/response/response.feature
+++ features/response/response.feature
@@ -4,7 +4,7 @@ Feature: Handling responses from the ID Broker API
     Given a call to "getUser" will return a 200 response with following data:
         """
         {
-          "employee_id":"123",
+          "employee_id": "123",
           "first_name": "John",
           "last_name": "Smith",
           "display_name": "John Smith",
@@ -13,6 +13,17 @@ Feature: Handling responses from the ID Broker API
           "locked": "no"
         }
         """
-    When I call getUser
+    When I call it with an "employee_id" of "123"
     Then the status code should be 200
-      And the response should be a "User" object
+      And the resulting "employee_id" should be "123"
+      And the resulting "first_name" should be "John"
+      And the resulting "last_name" should be "Smith"
+      And the resulting "display_name" should be "John Smith"
+      And the resulting "username" should be "john_smith"
+      And the resulting "email" should be "john_smith@example.com"
+      And the resulting "locked" should be "no"
+
+  Scenario: Getting a non-existent user
+    Given a call to "getUser" will return a 404 response
+    When I call it with an "employee_id" of "123"
+    Then the status code should be 404
